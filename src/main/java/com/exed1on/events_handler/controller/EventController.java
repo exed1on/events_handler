a0c8bfd076f8bd8e28588157f272bddc5737c2d8
yaroslavomelchenko <yomelchenko@gmail.com>
2023-01-19 15:40:17 +0100

    Prepared method to handle the number of expected matches given as a parameter

--- src/main/java/com/exed1on/events_handler/controller/EventController.java
+++ src/main/java/com/exed1on/events_handler/controller/EventController.java
@@ -1,20 +1,42 @@
 package com.exed1on.events_handler.controller;
 
+import com.exed1on.events_handler.repository.EventRepository;
 import com.exed1on.events_handler.service.EventsShowService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class EventController {
 
-   private final EventsShowService eventsShowService;
+    private final EventRepository eventRepository;
+    private final EventsShowService eventsShowService;
 
-    public EventController(EventsShowService eventsShowService) {
+    public EventController(EventRepository eventRepository, EventsShowService eventsShowService) {
+        this.eventRepository = eventRepository;
         this.eventsShowService = eventsShowService;
     }
 
+    private String result = "";
+
     @GetMapping("/events")
-    public String getAllEvents(){
-        return eventsShowService.getStringEvents(10);
+    public String getAllEvents(@RequestParam(required = false, defaultValue = "10") String matches, HttpServletResponse response) throws IOException {
+        int numberMatches;
+        try {
+            numberMatches = Integer.parseInt(matches);
+            if (numberMatches < 1) response.sendRedirect("/events?matches=1");
+            else if (numberMatches > eventRepository.count())
+                response.sendRedirect("/events?matches=" + eventRepository.count());
+            else {
+                result = eventsShowService.getStringEvents(numberMatches);
+            }
+        } catch (Exception e) {
+            response.sendRedirect("/events?matches=1");
+        }
+
+        return result;
     }
 }
